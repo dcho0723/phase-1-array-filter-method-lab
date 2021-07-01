86c02d621a27aae4c68079df384ecee05a9afe0c
dcho0723 <dcho0723@yahoo.com>
2021-06-30 20:16:05 -0400

    commit tree b05e9e95d85020a0313adaab7d4e7c40c6a9e70d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+// Code your solution her
+function findMatching(drivers, aStr) {
+    let findName = drivers.filter(function(driver) {
+        return driver.toLowerCase() === aStr.toLowerCase()
+    })
+    return findName;
+}
+
+const fuzzyMatch = (drivers, aStr) => {
+    let foundName = drivers.filter(function(driver) {
+        return aStr === driver.slice(0, 2)
+    }) 
+    return foundName;
+}
+
+const matchName = (driverObj, aStr) => {
+    let foundDriver = driverObj.filter(function(driver) {
+        return aStr === driverObj;
+    })
+    if (foundDriver.name === aStr)
+    return foundDriver;
+}
